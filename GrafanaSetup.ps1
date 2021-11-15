b55e3138fb8e8faa2b08e0203d21f6570124a6b1
Vikas Manchala <vikas.manchala@qualitestgroup.com>
2021-11-15 13:39:56 +0000

    updated grafana setup logic

--- GrafanaSetup.ps1
+++ GrafanaSetup.ps1
@@ -7,17 +7,23 @@ param (
 		
 	[Parameter(ParameterSetName='container')]
 	[string]$name = 'grafana',
-  [string]$cID = $(docker ps -qf "name=$name")
+  [string]$cID1 = $(docker ps -a -qf "name=$name" -f "status=exited"),
+  [string]$cID2 = $(docker ps -qf "name=$name")
 )
 
 
     # Check that container is running
-    if($cID){
-        Write-Output "The container $name is already running with ID: $cID"
+    if($cID1){
+        Write-Output "The container $name is already exist with ID: $cID1"
+        $(docker container start $cID1)       
+    }
+    elseif($cID2){
+        Write-Output "The container $name is already running with ID: $cID2"       
     }
     else {
-        Write-Output "Starting container $name...";
+        Write-Output "Starting container 2 $name...";
         docker volume create grafana-storage
         docker run -d -p 3000:3000 --name=grafana -v grafana-storage:/var/lib/grafana grafana/grafana-oss
     }
+
     
